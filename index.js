80923522d456aaea6cb07e7113ef81f704a877de
MD.Abdul Khalek Rasel <raselak101010@gmail.com>
2024-05-06 15:10:31 +0600

    mongodb,express,cors,env setup and menus get all menuItem done

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middle ware
+app.use(cors());
+app.use(express.json());
+
+// mongodb config
+
+const uri = `mongodb+srv://${process.env.SECRET_UserName}:${process.env.SECRET_Password}@cluster0.mc1hfkb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    // start our  code
+
+    // database Collection
+    const menuCollections = client.db("foody-client-db").collection("menus");
+    const cartCollections = client
+      .db("foody-client-db")
+      .collection("cartsItems");
+
+    // all menus items operations
+    app.get("/menu", async (req, res) => {
+      const result = await menuCollections.find().toArray();
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
